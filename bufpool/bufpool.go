0c85fbc6190bb8dfe7cac0658abd3cc34ad65365
Pierre Durand <pierredurand@gmail.com>
2022-01-05 16:09:59 +0100

    bufpool

---
+++ bufpool/bufpool.go
@@ -0,0 +1,48 @@
+// Package bufpool provides a sync.Pool of bytes.Buffer.
+package bufpool
+
+import (
+	"bytes"
+	"sync"
+)
+
+const maxCapDefault = 1 << 16 // 64 KiB
+
+// Pool is a pool of *bytes.Buffer.
+type Pool struct {
+	o sync.Once
+	p sync.Pool
+
+	// MaxCap defines the maximum capacity accepted for recycled buffer.
+	// If Put() is called with a buffer larger than this value, it's discarded.
+	// See https://github.com/golang/go/issues/23199 .
+	// 0 (default) means 64 KiB.
+	// A negative value means no limit.
+	MaxCap int
+}
+
+func (p *Pool) init() {
+	p.p.New = func() interface{} {
+		return new(bytes.Buffer)
+	}
+	if p.MaxCap == 0 {
+		p.MaxCap = maxCapDefault
+	}
+}
+
+// Get gets a buffer from the Pool, resets it and returns it.
+func (p *Pool) Get() *bytes.Buffer {
+	p.o.Do(p.init)
+	buf := p.p.Get().(*bytes.Buffer)
+	buf.Reset()
+	return buf
+}
+
+// Put puts the buffer to the Pool.
+// WARNING: the call MUST NOT reuse the buffer's content after this call.
+func (p *Pool) Put(buf *bytes.Buffer) {
+	p.o.Do(p.init)
+	if p.MaxCap < 0 || buf.Cap() <= p.MaxCap {
+		p.p.Put(buf)
+	}
+}
---
+++ bufpool/bufpool_test.go
@@ -0,0 +1,42 @@
+package bufpool
+
+import (
+	"bytes"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	p := &Pool{}
+	buf := p.Get()
+	p.Put(buf)
+}
+
+const testBenchData = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+var benchmarkResult string
+
+func BenchmarkBufPool(b *testing.B) {
+	p := &Pool{}
+	var s string
+	for i := 0; i < b.N; i++ {
+		buf := p.Get()
+		for j := 0; j < 10; j++ {
+			buf.WriteString(testBenchData)
+		}
+		s = buf.String()
+		p.Put(buf)
+	}
+	benchmarkResult = s
+}
+
+func BenchmarkBufWithoutPool(b *testing.B) {
+	var s string
+	for i := 0; i < b.N; i++ {
+		buf := new(bytes.Buffer)
+		for j := 0; j < 10; j++ {
+			buf.WriteString(testBenchData)
+		}
+		s = buf.String()
+	}
+	benchmarkResult = s
+}
