f5d10ea9fb7d643669f8c6b4d8ebb831ae1fa831
Pierre Durand <pierredurand@gmail.com>
2022-01-11 10:58:33 +0100

    closeutil

---
+++ closeutil/closeutil.go
@@ -0,0 +1,70 @@
+// Package closeutil provides close related utilities.
+package closeutil
+
+import (
+	"github.com/pierrre/go-libs/errors"
+)
+
+// F represents a function that closes something.
+type F func()
+
+// Nil returns a new F that checks if the F referenced by the method pointer receiver is not nil, and if so calls it.
+func (f *F) Nil() F {
+	return func() {
+		if *f != nil {
+			(*f)()
+		}
+	}
+}
+
+// Err represents a function that closes something and returns an error.
+type Err func() error
+
+// Convert converts the Err to a WithOnErr.
+func (f Err) Convert() WithOnErr {
+	return func(oe OnErr) {
+		err := f()
+		if err != nil {
+			oe(err)
+		}
+	}
+}
+
+// Nil returns a new Err that checks if the Err referenced by the method pointer receiver is not nil, and if so calls it.
+func (f *Err) Nil() Err {
+	return func() error {
+		if *f != nil {
+			return (*f)()
+		}
+		return nil
+	}
+}
+
+// OnErr represents a function that is called when an error occurs.
+type OnErr func(error)
+
+// Wrap wraps the OnErr with a message.
+func (f OnErr) Wrap(msg string) OnErr {
+	return func(err error) {
+		f(errors.Wrap(err, msg))
+	}
+}
+
+// WithOnErr represents a function that closes something and calls OnErr if an error occurs.
+type WithOnErr func(OnErr)
+
+// Wrap wraps the WithOnErr with a message.
+func (f WithOnErr) Wrap(msg string) WithOnErr {
+	return func(oe OnErr) {
+		f(oe.Wrap(msg))
+	}
+}
+
+// Nil returns a new WithOnErr that checks if the WithOnErr referenced by the method pointer receiver is not nil, and if so calls it.
+func (f *WithOnErr) Nil() WithOnErr {
+	return func(oe OnErr) {
+		if *f != nil {
+			(*f)(oe)
+		}
+	}
+}
---
+++ closeutil/closeutil_test.go
@@ -0,0 +1,159 @@
+package closeutil
+
+import (
+	"testing"
+
+	"github.com/pierrre/go-libs/errors"
+)
+
+func TestFNilNil(t *testing.T) {
+	var c1 F
+	c2 := c1.Nil()
+	c2()
+}
+
+func TestFNilNotNil(t *testing.T) {
+	var c1 F
+	c2 := c1.Nil()
+	called := false
+	c1 = func() {
+		called = true
+	}
+	c2()
+	if !called {
+		t.Fatal("not called")
+	}
+}
+
+func TestErrConvertWithError(t *testing.T) {
+	ceCalled := false
+	ce := Err(func() error {
+		ceCalled = true
+		return errors.New("error")
+	})
+	coe := ce.Convert()
+	oeCalled := false
+	oe := func(err error) {
+		oeCalled = true
+		if err == nil {
+			t.Fatal("no error")
+		}
+	}
+	coe(oe)
+	if !ceCalled {
+		t.Fatal("ce not called")
+	}
+	if !oeCalled {
+		t.Fatal("oe not called")
+	}
+}
+
+func TestErrConvertNoError(t *testing.T) {
+	ceCalled := false
+	ce := Err(func() error {
+		ceCalled = true
+		return nil
+	})
+	coe := ce.Convert()
+	oe := func(err error) {
+		t.Fatal("oe called")
+	}
+	coe(oe)
+	if !ceCalled {
+		t.Fatal("ce not called")
+	}
+}
+
+func TestErrNilNil(t *testing.T) {
+	var c1 Err
+	c2 := c1.Nil()
+	err := c2()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestErrNilNotNilWithError(t *testing.T) {
+	var c1 Err
+	c2 := c1.Nil()
+	called := false
+	c1 = func() error {
+		called = true
+		return errors.New("error")
+	}
+	err := c2()
+	if err == nil {
+		t.Fatal("no error")
+	}
+	if !called {
+		t.Fatal("not called")
+	}
+}
+
+func TestErrNilNotNilNoError(t *testing.T) {
+	var c1 Err
+	c2 := c1.Nil()
+	called := false
+	c1 = func() error {
+		called = true
+		return nil
+	}
+	err := c2()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !called {
+		t.Fatal("not called")
+	}
+}
+
+func TestOnErrWrap(t *testing.T) {
+	oe := OnErr(func(err error) {
+		msg := err.Error()
+		expectedMsg := "test: error"
+		if msg != expectedMsg {
+			t.Fatalf("unexpected message: got %q, want %q", msg, expectedMsg)
+		}
+	})
+	oe = oe.Wrap("test")
+	oe(errors.New("error"))
+}
+
+func TestWithOnErrWrap(t *testing.T) {
+	coe := WithOnErr(func(oe OnErr) {
+		oe(errors.New("error"))
+	})
+	coe = coe.Wrap("test")
+	oeCalled := false
+	oe := func(err error) {
+		oeCalled = true
+		msg := err.Error()
+		expectedMsg := "test: error"
+		if msg != expectedMsg {
+			t.Fatalf("unexpected message: got %q, want %q", msg, expectedMsg)
+		}
+	}
+	coe(oe)
+	if !oeCalled {
+		t.Fatal("oe not called")
+	}
+}
+
+func TestWithOnErrNilNil(t *testing.T) {
+	var c1 WithOnErr
+	c2 := c1.Nil()
+	c2(func(err error) {})
+}
+
+func TestWithOnErrNilNotNil(t *testing.T) {
+	var c1 WithOnErr
+	c2 := c1.Nil()
+	called := false
+	c1 = func(oe OnErr) {
+		called = true
+	}
+	c2(func(err error) {})
+	if !called {
+		t.Fatal("not called")
+	}
+}
