045e7e2dfeed263ef75d3b64b90a3504aabf88b6
Pierre Durand <pierredurand@gmail.com>
2023-06-27 00:00:13 +0200

    strconvio: optimize

--- strconvio/strconvio.go
+++ strconvio/strconvio.go
@@ -15,20 +15,21 @@ func WriteBool(w io.Writer, b bool) (int, error) {
 // WriteFloat writes the string representation of the float to the writer.
 func WriteFloat(w io.Writer, f float64, fmt byte, prec, bitSize int) (int, error) {
 	bp := bytesPool.Get().(*[]byte) //nolint:forcetypeassert // The pool only contains *[]byte.
-	defer bytesPool.Put(bp)
 	*bp = strconv.AppendFloat((*bp)[:0], f, fmt, prec, bitSize)
-	return w.Write(*bp) //nolint:wrapcheck // It's fine.
+	n, err := w.Write(*bp)
+	bytesPool.Put(bp)
+	return n, err //nolint:wrapcheck // It's fine.
 }
 
-// WriteInt writes the string representation of the integer to the writer.
 func WriteInt(w io.Writer, i int64, base int) (int, error) {
 	if 0 <= i && i < 100 && base == 10 {
 		return io.WriteString(w, strconv.FormatInt(i, base)) //nolint:wrapcheck // It's fine.
 	}
 	bp := bytesPool.Get().(*[]byte) //nolint:forcetypeassert // The pool only contains *[]byte.
-	defer bytesPool.Put(bp)
 	*bp = strconv.AppendInt((*bp)[:0], i, base)
-	return w.Write(*bp) //nolint:wrapcheck // It's fine.
+	n, err := w.Write(*bp)
+	bytesPool.Put(bp)
+	return n, err //nolint:wrapcheck // It's fine.
 }
 
 // WriteUint writes the string representation of the unsigned integer to the writer.
@@ -37,18 +38,25 @@ func WriteUint(w io.Writer, i uint64, base int) (int, error) {
 		return io.WriteString(w, strconv.FormatUint(i, base)) //nolint:wrapcheck // It's fine.
 	}
 	bp := bytesPool.Get().(*[]byte) //nolint:forcetypeassert // The pool only contains *[]byte.
-	defer bytesPool.Put(bp)
 	*bp = strconv.AppendUint((*bp)[:0], i, base)
-	return w.Write(*bp) //nolint:wrapcheck // It's fine.
+	n, err := w.Write(*bp)
+	bytesPool.Put(bp)
+	return n, err //nolint:wrapcheck // It's fine.
 }
 
 // WriteQuote writes the quoted string to the writer.
 func WriteQuote(w io.Writer, s string) (int, error) {
+	if s == "" {
+		return w.Write(emptyQuotes) //nolint:wrapcheck // It's fine.
+	}
 	bp := bytesPool.Get().(*[]byte) //nolint:forcetypeassert // The pool only contains *[]byte.
-	defer bytesPool.Put(bp)
 	*bp = strconv.AppendQuote((*bp)[:0], s)
-	return w.Write(*bp) //nolint:wrapcheck // It's fine.
+	n, err := w.Write(*bp)
+	bytesPool.Put(bp)
+	return n, err //nolint:wrapcheck // It's fine.
 }
+
+var emptyQuotes = []byte(`""`)
 
 var bytesPool = sync.Pool{
 	New: func() any {
