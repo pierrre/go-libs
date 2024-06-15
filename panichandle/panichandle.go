295e30e7cc1270789a59978fd627f93d0e897400
Pierre Durand <pierredurand@gmail.com>
2024-06-15 21:16:59 +0200

    feat(panichandle): add errorhandle support

--- panichandle/panichandle.go
+++ panichandle/panichandle.go
@@ -1,21 +1,44 @@
 // Package panichandle handles panic.
 package panichandle
 
-// Handler handles panic.
-//
-// By default there is no handler.
-var Handler func(r any)
+import (
+	"context"
+)
 
-// Recover recovers panic and call Handler.
+// Handle handles panic.
 //
-// If there is no handler, it doesn't recover.
+// By default there is no [Handler].
+var Handle Handler
+
+// Recover recovers panic and calls [Handle].
+//
+// If there is no [Handler], it doesn't recover.
 //
 // It should be called in defer.
-func Recover() {
-	if Handler != nil {
+func Recover(ctx context.Context) {
+	if Handle != nil {
 		r := recover()
 		if r != nil {
-			Handler(r)
+			Handle(ctx, r)
 		}
 	}
+}
+
+// Handler is a function that handles panic.
+type Handler func(ctx context.Context, r any)
+
+// ErrorHandler is a [Handler] that converts the recovered value to [error] with Convert, and calls Handler.
+//
+// If the recovered value is already an [error], Convert is not called.
+type ErrorHandler struct {
+	Handler func(ctx context.Context, err error)
+	Convert func(r any) error
+}
+
+func (h ErrorHandler) Handle(ctx context.Context, r any) {
+	err, ok := r.(error)
+	if !ok {
+		err = h.Convert(r)
+	}
+	h.Handler(ctx, err)
 }
--- panichandle/panichandle_test.go
+++ panichandle/panichandle_test.go
@@ -1,50 +1,86 @@
 package panichandle
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/pierrre/assert"
 )
 
 func TestRecoverNoPanicWithoutHandler(t *testing.T) {
-	Recover()
+	ctx := context.Background()
+	Recover(ctx)
 }
 
-func TestRecoverNoPanicWithandler(t *testing.T) {
+func TestRecoverNoPanicWitHandler(t *testing.T) {
+	ctx := context.Background()
 	defer func() {
-		Handler = nil
+		Handle = nil
 	}()
 	called := false
-	Handler = func(r any) {
+	Handle = func(ctx context.Context, r any) {
 		called = true
 	}
 	defer func() {
 		assert.False(t, called)
 	}()
-	Recover()
+	Recover(ctx)
 }
 
 func TestRecoverPanicWithoutHandler(t *testing.T) {
+	ctx := context.Background()
 	defer func() {
 		r := recover()
 		assert.NotZero(t, r)
 	}()
-	defer Recover()
+	defer Recover(ctx)
 	panic("test")
 }
 
 func TestRecoverPanicWithHandler(t *testing.T) {
+	ctx := context.Background()
 	defer func() {
-		Handler = nil
+		Handle = nil
 	}()
 	called := false
-	Handler = func(r any) {
+	Handle = func(ctx context.Context, r any) {
 		called = true
 		assert.NotZero(t, r)
 	}
 	defer func() {
 		assert.True(t, called)
 	}()
-	defer Recover()
+	defer Recover(ctx)
 	panic("test")
+}
+
+func TestErrorHandlerString(t *testing.T) {
+	ctx := context.Background()
+	called := false
+	h := ErrorHandler{
+		Handler: func(ctx context.Context, err error) {
+			called = true
+			assert.ErrorEqual(t, err, "error")
+		},
+		Convert: func(r any) error {
+			return fmt.Errorf("%s", r)
+		},
+	}
+	h.Handle(ctx, "error")
+	assert.True(t, called)
+}
+
+func TestErrorHandlerError(t *testing.T) {
+	ctx := context.Background()
+	called := false
+	h := ErrorHandler{
+		Handler: func(ctx context.Context, err error) {
+			called = true
+			assert.ErrorEqual(t, err, "error")
+		},
+	}
+	h.Handle(ctx, errors.New("error"))
+	assert.True(t, called)
 }
