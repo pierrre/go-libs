472e84951d6d164f05a87832707b4bc93182bb23
Pierre Durand <pierredurand@gmail.com>
2024-08-05 22:50:50 +0200

    fix reflectunsafe

--- reflectunsafe/reflectunsafe.go
+++ reflectunsafe/reflectunsafe.go
@@ -3,20 +3,34 @@ package reflectunsafe
 
 import (
 	"reflect"
+	"unsafe" //nolint:depguard // Required for this package.
 )
 
 // ConvertValueCanInterface converts a [reflect.Value] so it can be used with [reflect.Value.Interface].
 //
 // If [reflect.Value.CanInterface] returns true, it returns the original value.
 //
-// If [reflect.Value.CanAddr] returns false, it returns the original value.
+// If the value is not a pointer and [reflect.Value.CanAddr] returns false, it returns the original value.
 func ConvertValueCanInterface(v reflect.Value) reflect.Value {
 	if v.CanInterface() {
 		return v
 	}
-	if !v.CanAddr() {
+	typ := v.Type()
+	isPtr := false
+	var ptr unsafe.Pointer
+	switch {
+	case typ.Kind() == reflect.Ptr:
+		typ = typ.Elem()
+		isPtr = true
+		ptr = v.UnsafePointer()
+	case v.CanAddr():
+		ptr = v.Addr().UnsafePointer()
+	default:
 		return v
 	}
-	ptr := v.Addr().UnsafePointer()
-	return reflect.NewAt(v.Type(), ptr).Elem()
+	v = reflect.NewAt(typ, ptr)
+	if !isPtr {
+		v = v.Elem()
+	}
+	return v
 }
--- reflectunsafe/reflectunsafe_test.go
+++ reflectunsafe/reflectunsafe_test.go
@@ -11,36 +11,71 @@ import (
 func TestConvertValueCanInterfaceOK(t *testing.T) {
 	v := reflect.ValueOf(&testStruct{
 		unexported: "test",
-	}).Elem()
-	v = v.FieldByName("unexported")
+	}).Elem().FieldByName("unexported")
 	assert.False(t, v.CanInterface())
-	assert.True(t, v.CanAddr())
 	v = ConvertValueCanInterface(v)
 	assert.True(t, v.CanInterface())
-	assert.True(t, v.CanAddr())
+	vi := v.Interface()
+	s, _ := assert.Type[string](t, vi)
+	assert.Equal(t, s, "test")
+}
+
+func TestConvertValueCanInterfacePointer(t *testing.T) {
+	s1 := "test"
+	p1 := &s1
+	v := reflect.ValueOf(&testStruct{
+		pointer: p1,
+	}).Elem().FieldByName("pointer")
+	assert.False(t, v.CanInterface())
+	v = ConvertValueCanInterface(v)
+	assert.True(t, v.CanInterface())
+	vi := v.Interface()
+	p2, _ := assert.Type[*string](t, vi)
+	assert.NotZero(t, p2)
+	s2 := *p2
+	assert.Equal(t, s2, "test")
+}
+
+func TestConvertValueCanInterfacePointerPointer(t *testing.T) {
+	s1 := "test"
+	p1 := &s1
+	pp1 := &p1
+	v := reflect.ValueOf(&testStruct{
+		pointerPointer: pp1,
+	}).Elem().FieldByName("pointerPointer")
+	assert.False(t, v.CanInterface())
+	v = ConvertValueCanInterface(v)
+	assert.True(t, v.CanInterface())
+	vi := v.Interface()
+	pp2, _ := assert.Type[**string](t, vi)
+	assert.NotZero(t, pp2)
+	p2 := *pp2
+	assert.NotZero(t, p2)
+	s2 := *p2
+	assert.Equal(t, s2, "test")
 }
 
 func TestConvertValueCanInterfaceAlreadyOK(t *testing.T) {
 	v := reflect.ValueOf("test")
 	assert.True(t, v.CanInterface())
-	assert.False(t, v.CanAddr())
 	v = ConvertValueCanInterface(v)
 	assert.True(t, v.CanInterface())
-	assert.False(t, v.CanAddr())
+	vi := v.Interface()
+	s, _ := assert.Type[string](t, vi)
+	assert.Equal(t, s, "test")
 }
 
 func TestConvertValueCanInterfaceNotAddressable(t *testing.T) {
 	v := reflect.ValueOf(testStruct{
 		unexported: "test",
-	})
-	v = v.FieldByName("unexported")
+	}).FieldByName("unexported")
 	assert.False(t, v.CanInterface())
-	assert.False(t, v.CanAddr())
 	v = ConvertValueCanInterface(v)
 	assert.False(t, v.CanInterface())
-	assert.False(t, v.CanAddr())
 }
 
 type testStruct struct {
-	unexported string
+	unexported     string
+	pointer        *string
+	pointerPointer **string
 }
