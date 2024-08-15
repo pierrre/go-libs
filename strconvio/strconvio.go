fb9195f27572c4409640fe672d406fdc8078d6ea
Pierre Durand <pierredurand@gmail.com>
2024-08-15 16:36:17 +0200

    strconvio: use unsafeio

--- strconvio/strconvio.go
+++ strconvio/strconvio.go
@@ -6,11 +6,12 @@ import (
 	"strconv"
 
 	"github.com/pierrre/go-libs/syncutil"
+	"github.com/pierrre/go-libs/unsafeio"
 )
 
 // WriteBool writes the string representation of the bool to the writer.
 func WriteBool(w io.Writer, b bool) (int, error) {
-	return io.WriteString(w, strconv.FormatBool(b)) //nolint:wrapcheck // It's fine.
+	return unsafeio.WriteString(w, strconv.FormatBool(b)) //nolint:wrapcheck // It's fine.
 }
 
 // WriteFloat writes the string representation of the float to the writer.
@@ -25,7 +26,7 @@ func WriteFloat(w io.Writer, f float64, fmt byte, prec, bitSize int) (int, error
 // WriteInt writes the string representation of the signed integer to the writer.
 func WriteInt(w io.Writer, i int64, base int) (int, error) {
 	if 0 <= i && i < 100 && base == 10 {
-		return io.WriteString(w, strconv.FormatInt(i, base)) //nolint:wrapcheck // It's fine.
+		return unsafeio.WriteString(w, strconv.FormatInt(i, base)) //nolint:wrapcheck // It's fine.
 	}
 	bp := bytesPool.Get()
 	*bp = strconv.AppendInt((*bp)[:0], i, base)
@@ -37,7 +38,7 @@ func WriteInt(w io.Writer, i int64, base int) (int, error) {
 // WriteUint writes the string representation of the unsigned integer to the writer.
 func WriteUint(w io.Writer, i uint64, base int) (int, error) {
 	if i < 100 && base == 10 {
-		return io.WriteString(w, strconv.FormatUint(i, base)) //nolint:wrapcheck // It's fine.
+		return unsafeio.WriteString(w, strconv.FormatUint(i, base)) //nolint:wrapcheck // It's fine.
 	}
 	bp := bytesPool.Get()
 	*bp = strconv.AppendUint((*bp)[:0], i, base)
