9ffbc485bbbc32b39c518ffd17fe479c807fb5b2
Pierre Durand <pierredurand@gmail.com>
2024-09-14 16:57:07 +0200

    goroutine: add Iter

---
+++ goroutine/iter.go
@@ -0,0 +1,70 @@
+package goroutine
+
+import (
+	"context"
+	"iter"
+)
+
+// Func is a function that receives an In value and returns an Out value.
+type Func[In, Out any] func(ctx context.Context, v In) Out
+
+// Iter runs a function on an iterator with n concurrent workers.
+func Iter[In, Out any](ctx context.Context, in iter.Seq[In], n int, f Func[In, Out]) iter.Seq[Out] {
+	return func(yield func(Out) bool) {
+		ctx, cancel := context.WithCancel(ctx) //nolint:govet // Shadows ctx.
+		defer cancel()
+		inCh := make(chan In)
+		defer Wait(ctx, func(ctx context.Context) {
+			defer close(inCh)
+			for inV := range in {
+				inCh <- inV
+				select {
+				case <-ctx.Done():
+					return
+				default:
+				}
+			}
+		})()
+		stoppedOutIter := make(chan struct{})
+		outCh := make(chan Out)
+		defer Wait(ctx, func(ctx context.Context) {
+			defer close(outCh)
+			N(ctx, n, func(ctx context.Context, _ int) {
+				for inV := range inCh {
+					outV := f(ctx, inV)
+					select {
+					case outCh <- outV:
+					case <-stoppedOutIter:
+					}
+				}
+			})
+		})()
+		defer cancel()
+		defer close(stoppedOutIter)
+		for v := range outCh {
+			if !yield(v) {
+				return
+			}
+		}
+	}
+}
+
+// ValErr is a value with an error.
+type ValErr[T any] struct {
+	Val T
+	Err error
+}
+
+// ErrorFunc is a [Func] that also returns an error.
+type ErrorFunc[In, Out any] func(ctx context.Context, v In) (Out, error)
+
+// WithError transforms a [ErrorFunc] into a [Func].
+func WithError[In, Out any](f ErrorFunc[In, Out]) Func[In, ValErr[Out]] {
+	return func(ctx context.Context, v In) ValErr[Out] {
+		out, err := f(ctx, v)
+		return ValErr[Out]{
+			Val: out,
+			Err: err,
+		}
+	}
+}
---
+++ goroutine/iter_test.go
@@ -0,0 +1,172 @@
+package goroutine
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"slices"
+	"sync/atomic"
+	"testing"
+
+	"github.com/pierrre/assert"
+)
+
+func ExampleIter() {
+	ctx := context.Background()
+	in := slices.Values([]int{1, 2, 3, 4, 5})
+	out := Iter(ctx, in, 2, func(ctx context.Context, v int) int {
+		return v * 2
+	})
+	for v := range out {
+		fmt.Println(v)
+	}
+	// Unordered output:
+	// 2
+	// 4
+	// 6
+	// 8
+	// 10
+}
+
+func ExampleWithError() {
+	ctx := context.Background()
+	in := slices.Values([]int{1, 2, 3, 4, 5})
+	out := Iter(ctx, in, 2, WithError(func(ctx context.Context, v int) (int, error) {
+		if v == 3 {
+			return 0, errors.New("error")
+		}
+		return v * 2, nil
+	}))
+	for v := range out {
+		if v.Err != nil {
+			fmt.Println(v.Err)
+		} else {
+			fmt.Println(v.Val)
+		}
+	}
+	// Unordered output:
+	// 2
+	// 4
+	// error
+	// 8
+	// 10
+}
+
+const (
+	testIterCount = 100
+)
+
+var testIterInputInts = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+func runIterTest(t *testing.T, f func(t *testing.T)) {
+	t.Helper()
+	for range testIterCount {
+		f(t)
+	}
+}
+
+func TestIter(t *testing.T) {
+	ctx := context.Background()
+	in := slices.Values(testIterInputInts)
+	n := 2
+	f := func(ctx context.Context, v int) int {
+		return v * 2
+	}
+	out := Iter(ctx, in, n, f)
+	runIterTest(t, func(t *testing.T) { //nolint:thelper // This is not a helper.
+		res := slices.Collect(out)
+		slices.Sort(res)
+		expected := []int{2, 4, 6, 8, 10, 12, 14, 16, 18, 20}
+		assert.SliceEqual(t, res, expected)
+	})
+}
+
+func TestIterStopOutputIterator(t *testing.T) {
+	runIterTest(t, func(t *testing.T) { //nolint:thelper // This is not a helper.
+		ctx := context.Background()
+		in := slices.Values(testIterInputInts)
+		n := 2
+		workerCallcount := int64(0)
+		f := func(ctx context.Context, v int) int {
+			atomic.AddInt64(&workerCallcount, 1)
+			return v * 2
+		}
+		out := Iter(ctx, in, n, f)
+		iterCount := 0
+		for range out {
+			if iterCount >= 1 {
+				break
+			}
+			iterCount++
+		}
+		assert.LessOrEqual(t, workerCallcount, 5)
+	})
+}
+
+func TestIterContextCancel(t *testing.T) {
+	runIterTest(t, func(t *testing.T) { //nolint:thelper // This is not a helper.
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		in := slices.Values(testIterInputInts)
+		n := 2
+		workerCallcount := int64(0)
+		f := func(ctx context.Context, v int) int {
+			atomic.AddInt64(&workerCallcount, 1)
+			return v * 2
+		}
+		out := Iter(ctx, in, n, f)
+		iterCount := 0
+		for range out {
+			cancel()
+			iterCount++
+		}
+		assert.LessOrEqual(t, workerCallcount, 4)
+		assert.LessOrEqual(t, iterCount, 4)
+	})
+}
+
+func TestWithError(t *testing.T) {
+	runIterTest(t, func(t *testing.T) { //nolint:thelper // This is not a helper.
+		ctx := context.Background()
+		in := slices.Values(testIterInputInts)
+		n := 2
+		f := WithError(func(ctx context.Context, v int) (int, error) {
+			if v == 3 {
+				return 0, errors.New("error")
+			}
+			return v * 2, nil
+		})
+		out := Iter(ctx, in, n, f)
+		errCount := 0
+		for v := range out {
+			if v.Val == 0 {
+				errCount++
+				assert.Error(t, v.Err)
+			} else {
+				assert.NoError(t, v.Err)
+			}
+		}
+		assert.Equal(t, errCount, 1)
+	})
+}
+
+func BenchmarkIter(b *testing.B) {
+	ctx := context.Background()
+	in := func(yield func(int) bool) {
+		for i := range 100 {
+			if !yield(i) {
+				return
+			}
+		}
+	}
+	n := 2
+	f := func(ctx context.Context, v int) int {
+		return v * 2
+	}
+	b.ResetTimer()
+	for range b.N {
+		out := Iter(ctx, in, n, f)
+		for range out {
+		}
+	}
+}
