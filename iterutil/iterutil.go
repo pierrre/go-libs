9aeddafb7e97efb4552e51f4eb8780099c5fecbe
Pierre Durand <pierredurand@gmail.com>
2024-10-01 10:17:21 +0200

    iterutil: add doc

--- iterutil/iterutil.go
+++ iterutil/iterutil.go
@@ -5,6 +5,7 @@ import (
 	"iter"
 )
 
+// SeqToSeq2 converts a [iter.Seq] to a [iter.Seq2].
 func SeqToSeq2[In, Out1, Out2 any](in iter.Seq[In], convert func(In) (Out1, Out2)) iter.Seq2[Out1, Out2] {
 	return func(yield func(Out1, Out2) bool) {
 		for i := range in {
@@ -16,6 +17,7 @@ func SeqToSeq2[In, Out1, Out2 any](in iter.Seq[In], convert func(In) (Out1, Out2
 	}
 }
 
+// Seq2ToSeq converts a [iter.Seq2] to a [iter.Seq].
 func Seq2ToSeq[In1, In2, Out any](in iter.Seq2[In1, In2], convert func(In1, In2) Out) iter.Seq[Out] {
 	return func(yield func(Out) bool) {
 		for i1, i2 := range in {
@@ -26,11 +28,13 @@ func Seq2ToSeq[In1, In2, Out any](in iter.Seq2[In1, In2], convert func(In1, In2)
 	}
 }
 
+// KeyVal represents a key-value pair.
 type KeyVal[K, V any] struct {
 	Key K
 	Val V
 }
 
+// NewKeyVal creates a new [KeyVal].
 func NewKeyVal[K, V any](key K, val V) KeyVal[K, V] {
 	return KeyVal[K, V]{
 		Key: key,
@@ -38,6 +42,7 @@ func NewKeyVal[K, V any](key K, val V) KeyVal[K, V] {
 	}
 }
 
+// Values returns the key and value of the [KeyVal].
 func (kv KeyVal[K, V]) Values() (key K, val V) {
 	return kv.Key, kv.Val
 }
