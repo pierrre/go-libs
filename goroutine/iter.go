f4d90592a804d49212897a6b4cec26b4abc96cb2
Pierre Durand <pierredurand@gmail.com>
2024-10-05 22:32:29 +0200

    goroutine: clean Iter()

--- goroutine/iter.go
+++ goroutine/iter.go
@@ -19,32 +19,26 @@ func Iter[In, Out any](ctx context.Context, in iter.Seq[In], workers int, f func
 	}
 }
 
-// iterSeq runs the iterator logic.
-//
-// It is made up 3 components: producer, workers, and consumer.
-// The producer reads the input iterator and sends the input values to the workers.
-// The workers run the function for each input values with a goroutines pool, and send the output values to the consumer.
-// The consumer receives the output values from the workers, and sends them to the output iterator.
 func iterSeq[In, Out any](ctx context.Context, in iter.Seq[In], workers int, f func(context.Context, In) Out, yield func(Out) bool) {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
+	workers = max(workers, 1)
 	inCh := make(chan In)
 	outCh := make(chan Out)
 	workersWg := new(sync.WaitGroup)
-	defer workersWg.Wait()                 // Wait for all workers to finish.
-	consumerStopped := make(chan struct{}) // Used to notify that the consumer has stopped.
+	defer workersWg.Wait()
+	consumerStopped := make(chan struct{})
 	go iterProducer(ctx, in, inCh)
 	iterWorkers(ctx, workersWg, workers, f, inCh, outCh, consumerStopped)
 	iterConsumer(cancel, consumerStopped, outCh, yield)
 }
 
 func iterProducer[In any](ctx context.Context, in iter.Seq[In], inCh chan<- In) {
-	defer close(inCh) // Notify the workers that there are no more input values.
+	defer close(inCh)
 	for inV := range in {
 		inCh <- inV
 		select {
 		case <-ctx.Done():
-			// The context is canceled, stop the producer.
 			return
 		default:
 		}
@@ -52,27 +46,25 @@ func iterProducer[In any](ctx context.Context, in iter.Seq[In], inCh chan<- In) 
 }
 
 func iterWorkers[In, Out any](ctx context.Context, wg *sync.WaitGroup, workers int, f func(context.Context, In) Out, inCh <-chan In, outCh chan<- Out, consumerStopped <-chan struct{}) {
-	// Start all workers.
-	workers = max(workers, 1) // Ensure there is at least 1 worker.
 	wg.Add(workers)
 	for range workers {
 		go iterWorker(ctx, wg, f, inCh, outCh, consumerStopped)
 	}
 	go func() {
-		wg.Wait()    // Wait for all workers to finish.
-		close(outCh) // Notify the consumer that there are no more output values.
+		wg.Wait()
+		close(outCh)
 	}()
 }
 
 func iterWorker[In, Out any](ctx context.Context, wg *sync.WaitGroup, f func(context.Context, In) Out, inCh <-chan In, outCh chan<- Out, consumerStopped <-chan struct{}) {
-	defer wg.Done()         // Notify that the worker has finished.
-	for inV := range inCh { // Receive the input value from the producer.
+	defer wg.Done()
+	for inV := range inCh {
 		func() {
 			defer panichandle.Recover(ctx)
 			outV := f(ctx, inV)
 			select {
-			case outCh <- outV: // Send the output value to the consumer.
-			case <-consumerStopped: // The consumer has stopped, discard the output value.
+			case outCh <- outV:
+			case <-consumerStopped:
 			}
 		}()
 	}
@@ -80,11 +72,11 @@ func iterWorker[In, Out any](ctx context.Context, wg *sync.WaitGroup, f func(con
 
 func iterConsumer[Out any](cancel context.CancelFunc, consumerStopped chan<- struct{}, outCh <-chan Out, yield func(Out) bool) {
 	defer func() {
-		cancel()               // Notify the producer to stop.
-		close(consumerStopped) // Notify the workers that the consumer has stopped.
+		cancel()
+		close(consumerStopped)
 	}()
-	for outV := range outCh { // Receive the output value from the workers.
-		if !yield(outV) { // Send the output value to the output iterator.
+	for outV := range outCh {
+		if !yield(outV) {
 			return
 		}
 	}
