96eafafcb31a915cf089f71295187dcfb6471d4e
Pierre Durand <pierredurand@gmail.com>
2025-05-03 09:52:00 +0200

    worker

---
+++ worker/worker.go
@@ -0,0 +1,132 @@
+// Package worker provides a way to run a function in a loop, with intervals and error handling.
+package worker
+
+import (
+	"context"
+	"time"
+)
+
+// Func represents a function.
+type Func func(ctx context.Context)
+
+// Run runs the [Func] in a loop until the [context.Context] is done.
+func Run(ctx context.Context, f Func, opts ...Option) {
+	o := buildOptions(opts...)
+	var ticker *time.Ticker
+	if o.interval > 0 {
+		ticker = time.NewTicker(o.interval)
+		defer ticker.Stop()
+	}
+	for ctx.Err() == nil {
+		if o.immediately {
+			f(ctx)
+		} else {
+			o.immediately = true
+		}
+		if ticker == nil {
+			continue
+		}
+		if o.fixed {
+			tm := time.Now()
+			tm = tm.Truncate(o.interval)
+			tm = tm.Add(o.interval)
+			d := time.Until(tm)
+			sleep(ctx, d)
+			ticker.Reset(o.interval)
+			o.fixed = false
+			continue
+		}
+		waitChan(ctx, ticker.C)
+	}
+}
+
+type options struct {
+	interval    time.Duration
+	immediately bool
+	fixed       bool
+}
+
+func buildOptions(opts ...Option) *options {
+	o := &options{
+		immediately: true,
+	}
+	for _, opt := range opts {
+		opt(o)
+	}
+	return o
+}
+
+// Option is an option for [Run].
+type Option func(*options)
+
+// WithInterval sets the interval between each function call.
+// The default value is 0, which means the function will be called without any delay between calls.
+func WithInterval(d time.Duration) Option {
+	return func(o *options) {
+		o.interval = d
+	}
+}
+
+// WithImmediately sets whether to call the function immediately before the first interval.
+// The default value is true, which means the function will be called immediately.
+func WithImmediately(b bool) Option {
+	return func(o *options) {
+		o.immediately = b
+	}
+}
+
+// WithFixed sets whether to call the function at fixed times.
+// E.g. if the interval is 1 minute, it will call the function every minute at 0s.
+func WithFixed(b bool) Option {
+	return func(o *options) {
+		o.fixed = b
+	}
+}
+
+// ErrorFunc represents a function that returns an error.
+type ErrorFunc func(ctx context.Context) error
+
+// NewFuncWithError creates a new [Func] that calls the given [ErrorFunc].
+// If no error is returned, it will return.
+// If an error is returned, it will call the given [OnErrorFunc] if it is not nil.
+// If the retry parameter is true, it will retry the [ErrorFunc] until it returns no error or the context is done.
+func NewFuncWithError(ctx context.Context, ef ErrorFunc, onError OnErrorFunc, retry bool) Func {
+	return func(ctx context.Context) {
+		for ctx.Err() == nil {
+			err := ef(ctx)
+			if err == nil {
+				return
+			}
+			if onError != nil {
+				onError(ctx, err)
+			}
+			if !retry {
+				return
+			}
+		}
+	}
+}
+
+// OnErrorFunc represents a function that handles an error.
+type OnErrorFunc func(ctx context.Context, err error)
+
+// NewOnErrorFuncWithDelay creates a new [OnErrorFunc] that calls the given [OnErrorFunc], then waits for the given duration.
+func NewOnErrorFuncWithDelay(onError OnErrorFunc, d time.Duration) OnErrorFunc {
+	return func(ctx context.Context, err error) {
+		onError(ctx, err)
+		sleep(ctx, d)
+	}
+}
+
+func sleep(ctx context.Context, d time.Duration) {
+	tm := time.NewTimer(d)
+	defer tm.Stop()
+	waitChan(ctx, tm.C)
+}
+
+func waitChan(ctx context.Context, ch <-chan time.Time) {
+	select {
+	case <-ch:
+	case <-ctx.Done():
+	}
+}
---
+++ worker/worker_test.go
@@ -0,0 +1,158 @@
+package worker
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/pierrre/assert"
+)
+
+func Example() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	called := 0
+	ef := func(ctx context.Context) error {
+		called++
+		fmt.Println("call:", called)
+		if called <= 5 {
+			return errors.New("error")
+		}
+		if called >= 10 {
+			cancel()
+		}
+		return nil
+	}
+
+	onError := func(ctx context.Context, err error) {
+		fmt.Println("on error:", err)
+	}
+	onError = NewOnErrorFuncWithDelay(onError, 1*time.Microsecond)
+
+	f := NewFuncWithError(ctx, ef, onError, true)
+
+	Run(ctx, f, WithImmediately(true), WithInterval(10*time.Microsecond), WithFixed(true))
+
+	// Output:
+	// call: 1
+	// on error: error
+	// call: 2
+	// on error: error
+	// call: 3
+	// on error: error
+	// call: 4
+	// on error: error
+	// call: 5
+	// on error: error
+	// call: 6
+	// call: 7
+	// call: 8
+	// call: 9
+	// call: 10
+}
+
+func TestRun(t *testing.T) {
+	ctx := t.Context()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	called := 0
+	f := func(ctx context.Context) {
+		called++
+		if called == 10 {
+			cancel()
+		}
+	}
+	Run(ctx, f)
+	assert.Equal(t, called, 10)
+}
+
+func TestRunWithInterval(t *testing.T) {
+	ctx := t.Context()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	called := 0
+	f := func(ctx context.Context) {
+		called++
+		if called == 10 {
+			cancel()
+		}
+	}
+	Run(ctx, f, WithInterval(1*time.Microsecond))
+	assert.Equal(t, called, 10)
+}
+
+func TestRunWithImmediatelyFalse(t *testing.T) {
+	ctx := t.Context()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	called := 0
+	f := func(ctx context.Context) {
+		called++
+		if called == 10 {
+			cancel()
+		}
+	}
+	Run(ctx, f, WithImmediately(false))
+	assert.Equal(t, called, 10)
+}
+
+func TestRunWithFixed(t *testing.T) {
+	ctx := t.Context()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	called := 0
+	f := func(ctx context.Context) {
+		called++
+		if called == 10 {
+			cancel()
+		}
+	}
+	Run(ctx, f, WithInterval(1*time.Microsecond), WithFixed(true))
+	assert.Equal(t, called, 10)
+}
+
+func TestNewFuncWithError(t *testing.T) {
+	ctx := t.Context()
+	called := 0
+	ef := func(ctx context.Context) error {
+		called++
+		if called < 10 {
+			return errors.New("error")
+		}
+		return nil
+	}
+	onErrorCalled := 0
+	onError := func(ctx context.Context, err error) {
+		onErrorCalled++
+	}
+	f := NewFuncWithError(ctx, ef, onError, true)
+	f(ctx)
+	assert.Equal(t, onErrorCalled, 9)
+}
+
+func TestNewFuncWithErrorNoRetry(t *testing.T) {
+	ctx := t.Context()
+	called := 0
+	ef := func(ctx context.Context) error {
+		called++
+		return errors.New("error")
+	}
+	f := NewFuncWithError(ctx, ef, nil, false)
+	f(ctx)
+	assert.Equal(t, called, 1)
+}
+
+func TestNewOnErrorFuncWithDelay(t *testing.T) {
+	ctx := t.Context()
+	called := 0
+	onError := func(ctx context.Context, err error) {
+		called++
+	}
+	onError = NewOnErrorFuncWithDelay(onError, 1*time.Microsecond)
+	onError(ctx, errors.New("error"))
+	assert.Equal(t, called, 1)
+}
