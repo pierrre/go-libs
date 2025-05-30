657489aa0c4d525a85753ab1722480b11ce2c737
Pierre Durand <pierredurand@gmail.com>
2025-05-30 21:44:30 +0200

    iterutil: add SeqToSeq2Index() + Seq2ToSeqKey() + Seq2ToSeqValue()

--- iterutil/iterutil.go
+++ iterutil/iterutil.go
@@ -17,6 +17,20 @@ func SeqToSeq2[In, Out1, Out2 any](in iter.Seq[In], convert func(In) (Out1, Out2
 	}
 }
 
+// SeqToSeq2Index converts a [iter.Seq] to a [iter.Seq2] that yields the index and value.
+// The index is the position of the value in the sequence, starting from 0.
+func SeqToSeq2Index[V any](in iter.Seq[V]) iter.Seq2[int, V] {
+	return func(yield func(int, V) bool) {
+		i := 0
+		for v := range in {
+			if !yield(i, v) {
+				break
+			}
+			i++
+		}
+	}
+}
+
 // Seq2ToSeq converts a [iter.Seq2] to a [iter.Seq].
 func Seq2ToSeq[In1, In2, Out any](in iter.Seq2[In1, In2], convert func(In1, In2) Out) iter.Seq[Out] {
 	return func(yield func(Out) bool) {
@@ -26,6 +40,20 @@ func Seq2ToSeq[In1, In2, Out any](in iter.Seq2[In1, In2], convert func(In1, In2)
 			}
 		}
 	}
+}
+
+// Seq2ToSeqKey converts a [iter.Seq2] to a [iter.Seq] that yields only the keys.
+func Seq2ToSeqKey[K, V any](seq iter.Seq2[K, V]) iter.Seq[K] {
+	return Seq2ToSeq(seq, func(k K, _ V) K {
+		return k
+	})
+}
+
+// Seq2ToSeqValue converts a [iter.Seq2] to a [iter.Seq] that yields only the values.
+func Seq2ToSeqValue[K, V any](seq iter.Seq2[K, V]) iter.Seq[V] {
+	return Seq2ToSeq(seq, func(_ K, v V) V {
+		return v
+	})
 }
 
 // KeyVal represents a key-value pair.
--- iterutil/iterutil_test.go
+++ iterutil/iterutil_test.go
@@ -61,6 +61,23 @@ func TestSeqToSeq2Stop(t *testing.T) {
 	}
 }
 
+func TestSeqToSeq2Index(t *testing.T) {
+	ss := []string{"zero", "one", "two"}
+	it := SeqToSeq2Index(slices.Values(ss))
+	for k, v := range it {
+		assert.Equal(t, v, ss[k])
+	}
+}
+
+func TestSeqToSeq2IndexStop(t *testing.T) {
+	ss := []string{"zero", "one", "two"}
+	it := SeqToSeq2Index(slices.Values(ss))
+	for k, v := range it {
+		assert.Equal(t, v, ss[k])
+		break
+	}
+}
+
 func TestSeq2ToSeq(t *testing.T) {
 	ss := []string{"zero", "one", "two"}
 	it := Seq2ToSeq(slices.All(ss), NewKeyVal)
@@ -76,4 +93,26 @@ func TestSeq2ToSeqStop(t *testing.T) {
 		assert.Equal(t, kv.Val, ss[kv.Key])
 		break
 	}
+}
+
+func TestSeq2ToSeqKey(t *testing.T) {
+	ss := []string{"zero", "one", "two"}
+	it := Seq2ToSeqKey(slices.All(ss))
+	expected := 0
+	for k := range it {
+		assert.Equal(t, k, expected)
+		expected++
+	}
+	assert.Equal(t, expected, len(ss))
+}
+
+func TestSeq2ToSeqValue(t *testing.T) {
+	ss := []string{"zero", "one", "two"}
+	it := Seq2ToSeqValue(slices.All(ss))
+	i := 0
+	for v := range it {
+		assert.Equal(t, v, ss[i])
+		i++
+	}
+	assert.Equal(t, i, len(ss))
 }
