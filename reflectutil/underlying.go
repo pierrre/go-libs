0bdf5deda2a6715c77503708e16d9ab0022cf70c
Pierre Durand <pierredurand@gmail.com>
2025-06-10 15:28:10 +0200

    reflectutil: rename "base type" to "underlying type"

--- reflectutil/underlying.go
+++ reflectutil/underlying.go
@@ -7,29 +7,36 @@ import (
 	"github.com/pierrre/go-libs/syncutil"
 )
 
-// GetBaseType returns the base type of the given type.
+// GeUnderlyingType returns the underlying type of the given type.
 // E.g. for the type defined as `type MyType string`, it returns `string`.
-// It returns the type itself if it's already a base type, or if the kind is invalid, interface, or struct.
-func GetBaseType(typ reflect.Type) reflect.Type {
+// It returns the type itself if it's its own underlying type, or if the kind is invalid/interface/struct.
+func GeUnderlyingType(typ reflect.Type) reflect.Type {
 	kind := typ.Kind()
-	baseType := knownKindBaseTypes[kind]
-	if baseType != nil {
-		return baseType
+	uTyp := knownKindUnderlyingTypes[kind]
+	if uTyp != nil {
+		return uTyp
 	}
 	switch kind { //nolint:exhaustive // We only need these kinds.
 	case reflect.Invalid, reflect.Interface, reflect.Struct:
 		return typ
 	}
-	baseType, ok := baseTypeCache.Load(typ)
+	uTyp, ok := underlyingTypeCache.Load(typ)
 	if ok {
-		return baseType
+		return uTyp
 	}
-	baseType = computeBaseType(typ)
-	baseTypeCache.Store(typ, baseType)
-	return baseType
+	uTyp = computeUnderlyingType(typ)
+	underlyingTypeCache.Store(typ, uTyp)
+	return uTyp
 }
 
-var knownKindBaseTypes = [...]reflect.Type{
+// GetBaseType is an alias for [GeUnderlyingType].
+//
+// Deprecated: use [GeUnderlyingType] instead.
+func GetBaseType(typ reflect.Type) reflect.Type {
+	return GeUnderlyingType(typ)
+}
+
+var knownKindUnderlyingTypes = [...]reflect.Type{
 	reflect.Bool:          reflect.TypeFor[bool](),
 	reflect.Int:           reflect.TypeFor[int](),
 	reflect.Int8:          reflect.TypeFor[int8](),
@@ -50,15 +57,15 @@ var knownKindBaseTypes = [...]reflect.Type{
 	reflect.UnsafePointer: reflect.TypeFor[unsafe.Pointer](),
 }
 
-var baseTypeCache syncutil.Map[reflect.Type, reflect.Type]
+var underlyingTypeCache syncutil.Map[reflect.Type, reflect.Type]
 
-func computeBaseType(typ reflect.Type) reflect.Type {
-	baseType := typ
+func computeUnderlyingType(typ reflect.Type) reflect.Type {
+	uTyp := typ
 	switch typ.Kind() { //nolint:exhaustive // We only need these kinds.
 	case reflect.Array:
-		baseType = reflect.ArrayOf(typ.Len(), typ.Elem())
+		uTyp = reflect.ArrayOf(typ.Len(), typ.Elem())
 	case reflect.Chan:
-		baseType = reflect.ChanOf(typ.ChanDir(), typ.Elem())
+		uTyp = reflect.ChanOf(typ.ChanDir(), typ.Elem())
 	case reflect.Func:
 		in := make([]reflect.Type, typ.NumIn())
 		for i := range in {
@@ -68,13 +75,13 @@ func computeBaseType(typ reflect.Type) reflect.Type {
 		for i := range out {
 			out[i] = typ.Out(i)
 		}
-		baseType = reflect.FuncOf(in, out, typ.IsVariadic())
+		uTyp = reflect.FuncOf(in, out, typ.IsVariadic())
 	case reflect.Map:
-		baseType = reflect.MapOf(typ.Key(), typ.Elem())
+		uTyp = reflect.MapOf(typ.Key(), typ.Elem())
 	case reflect.Pointer:
-		baseType = reflect.PointerTo(typ.Elem())
+		uTyp = reflect.PointerTo(typ.Elem())
 	case reflect.Slice:
-		baseType = reflect.SliceOf(typ.Elem())
+		uTyp = reflect.SliceOf(typ.Elem())
 	}
-	return baseType
+	return uTyp
 }
