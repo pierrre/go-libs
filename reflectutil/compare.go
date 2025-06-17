95b69b29a8a5a9311783e17633dd32abdd407f46
Pierre Durand <pierredurand@gmail.com>
2025-06-17 18:14:44 +0200

    reflectutil: optimize compare

--- reflectutil/compare.go
+++ reflectutil/compare.go
@@ -5,6 +5,8 @@ import (
 	"cmp"
 	"reflect"
 	"strings"
+
+	"github.com/pierrre/go-libs/syncutil"
 )
 
 // Compare compares two values of the same [reflect.Type].
@@ -13,55 +15,56 @@ func Compare(a, b reflect.Value) int {
 	if kind == b.Kind() {
 		typ := a.Type()
 		if typ == b.Type() {
-			return getCompareFunc(typ, kind)(a, b)
+			return GetCompareFunc(typ)(a, b)
 		}
 	}
 	return -1
 }
 
-// GetCompareFunc returns a function that compares two values of the given [reflect.Type].
-//
-// The returned function panics if the [reflect.Type] is not supported.
-func GetCompareFunc(typ reflect.Type) func(a, b reflect.Value) int {
-	return getCompareFunc(typ, typ.Kind())
-}
+// CompareFunc represents a function that compares 2 [reflect.Value] of the same [reflect.Type].
+type CompareFunc func(a, b reflect.Value) int
 
-func getCompareFunc(typ reflect.Type, kind reflect.Kind) func(a, b reflect.Value) int {
+// GetCompareFunc returns a function that compares two values of the given [reflect.Type].
+func GetCompareFunc(typ reflect.Type) CompareFunc {
 	if typ == bytesType {
 		return compareBytes
 	}
-	return kindCompareFuncs[kind]
+	return kindGetCompareFuncs[typ.Kind()](typ)
 }
 
-var kindCompareFuncs [reflect.UnsafePointer + 1]func(a, b reflect.Value) int
+var kindGetCompareFuncs [reflect.UnsafePointer + 1]func(typ reflect.Type) CompareFunc
 
 func init() {
-	kindCompareFuncs[reflect.Bool] = compareBool
-	kindCompareFuncs[reflect.Int] = compareInt
-	kindCompareFuncs[reflect.Int8] = compareInt
-	kindCompareFuncs[reflect.Int16] = compareInt
-	kindCompareFuncs[reflect.Int32] = compareInt
-	kindCompareFuncs[reflect.Int64] = compareInt
-	kindCompareFuncs[reflect.Uint] = compareUint
-	kindCompareFuncs[reflect.Uint8] = compareUint
-	kindCompareFuncs[reflect.Uint16] = compareUint
-	kindCompareFuncs[reflect.Uint32] = compareUint
-	kindCompareFuncs[reflect.Uint64] = compareUint
-	kindCompareFuncs[reflect.Uintptr] = compareUint
-	kindCompareFuncs[reflect.Float32] = compareFloat
-	kindCompareFuncs[reflect.Float64] = compareFloat
-	kindCompareFuncs[reflect.Complex64] = compareComplex
-	kindCompareFuncs[reflect.Complex128] = compareComplex
-	kindCompareFuncs[reflect.Array] = compareArray
-	kindCompareFuncs[reflect.Chan] = comparePointer
-	kindCompareFuncs[reflect.Func] = comparePointer
-	kindCompareFuncs[reflect.Interface] = compareInterface
-	kindCompareFuncs[reflect.Map] = comparePointer
-	kindCompareFuncs[reflect.Pointer] = comparePointer
-	kindCompareFuncs[reflect.Slice] = comparePointer
-	kindCompareFuncs[reflect.String] = compareString
-	kindCompareFuncs[reflect.Struct] = compareStruct
-	kindCompareFuncs[reflect.UnsafePointer] = comparePointer
+	kindGetCompareFuncs[reflect.Bool] = getCompareFuncBool
+	kindGetCompareFuncs[reflect.Int] = getCompareFuncInt
+	kindGetCompareFuncs[reflect.Int8] = getCompareFuncInt
+	kindGetCompareFuncs[reflect.Int16] = getCompareFuncInt
+	kindGetCompareFuncs[reflect.Int32] = getCompareFuncInt
+	kindGetCompareFuncs[reflect.Int64] = getCompareFuncInt
+	kindGetCompareFuncs[reflect.Uint] = getCompareFuncUint
+	kindGetCompareFuncs[reflect.Uint8] = getCompareFuncUint
+	kindGetCompareFuncs[reflect.Uint16] = getCompareFuncUint
+	kindGetCompareFuncs[reflect.Uint32] = getCompareFuncUint
+	kindGetCompareFuncs[reflect.Uint64] = getCompareFuncUint
+	kindGetCompareFuncs[reflect.Uintptr] = getCompareFuncUint
+	kindGetCompareFuncs[reflect.Float32] = getCompareFuncFloat
+	kindGetCompareFuncs[reflect.Float64] = getCompareFuncFloat
+	kindGetCompareFuncs[reflect.Complex64] = getCompareFuncComplex
+	kindGetCompareFuncs[reflect.Complex128] = getCompareFuncComplex
+	kindGetCompareFuncs[reflect.Array] = getCompareFuncArray
+	kindGetCompareFuncs[reflect.Chan] = getCompareFuncPointer
+	kindGetCompareFuncs[reflect.Func] = getCompareFuncPointer
+	kindGetCompareFuncs[reflect.Interface] = getCompareFuncInterface
+	kindGetCompareFuncs[reflect.Map] = getCompareFuncPointer
+	kindGetCompareFuncs[reflect.Pointer] = getCompareFuncPointer
+	kindGetCompareFuncs[reflect.Slice] = getCompareFuncPointer // TODO improve
+	kindGetCompareFuncs[reflect.String] = getCompareFuncString
+	kindGetCompareFuncs[reflect.Struct] = getCompareFuncStruct
+	kindGetCompareFuncs[reflect.UnsafePointer] = getCompareFuncPointer
+}
+
+func getCompareFuncBool(typ reflect.Type) CompareFunc {
+	return compareBool
 }
 
 func compareBool(a, b reflect.Value) int {
@@ -76,16 +79,32 @@ func compareBool(a, b reflect.Value) int {
 	}
 }
 
+func getCompareFuncInt(typ reflect.Type) CompareFunc {
+	return compareInt
+}
+
 func compareInt(a, b reflect.Value) int {
 	return cmp.Compare(a.Int(), b.Int())
+}
+
+func getCompareFuncUint(typ reflect.Type) CompareFunc {
+	return compareUint
 }
 
 func compareUint(a, b reflect.Value) int {
 	return cmp.Compare(a.Uint(), b.Uint())
 }
 
+func getCompareFuncFloat(typ reflect.Type) CompareFunc {
+	return compareFloat
+}
+
 func compareFloat(a, b reflect.Value) int {
 	return cmp.Compare(a.Float(), b.Float())
+}
+
+func getCompareFuncComplex(typ reflect.Type) CompareFunc {
+	return compareComplex
 }
 
 func compareComplex(a, b reflect.Value) int {
@@ -97,15 +116,29 @@ func compareComplex(a, b reflect.Value) int {
 	)
 }
 
-func compareArray(a, b reflect.Value) int {
-	elemCmp := GetCompareFunc(a.Type().Elem())
-	for i := range a.Len() {
-		c := elemCmp(a.Index(i), b.Index(i))
-		if c != 0 {
-			return c
-		}
+func getCompareFuncArray(typ reflect.Type) CompareFunc {
+	f, ok := compareFuncArrayCache.Load(typ)
+	if ok {
+		return f
 	}
-	return 0
+	elemCmp := GetCompareFunc(typ.Elem())
+	f = func(a, b reflect.Value) int {
+		for i := range a.Len() {
+			c := elemCmp(a.Index(i), b.Index(i))
+			if c != 0 {
+				return c
+			}
+		}
+		return 0
+	}
+	compareFuncArrayCache.Store(typ, f)
+	return f
+}
+
+var compareFuncArrayCache syncutil.Map[reflect.Type, CompareFunc]
+
+func getCompareFuncInterface(typ reflect.Type) CompareFunc {
+	return compareInterface
 }
 
 func compareInterface(a, b reflect.Value) int {
@@ -120,6 +153,10 @@ func compareInterface(a, b reflect.Value) int {
 	return Compare(a.Elem(), b.Elem())
 }
 
+func getCompareFuncPointer(typ reflect.Type) CompareFunc {
+	return comparePointer
+}
+
 func comparePointer(a, b reflect.Value) int {
 	return cmp.Compare(uintptr(a.UnsafePointer()), uintptr(b.UnsafePointer()))
 }
@@ -130,20 +167,39 @@ func compareBytes(a, b reflect.Value) int {
 	return bytes.Compare(a.Bytes(), b.Bytes())
 }
 
+func getCompareFuncString(typ reflect.Type) CompareFunc {
+	return compareString
+}
+
 func compareString(a, b reflect.Value) int {
 	return strings.Compare(a.String(), b.String())
 }
 
-func compareStruct(a, b reflect.Value) int {
-	for i := range a.NumField() {
-		af, bf := a.Field(i), b.Field(i)
-		c := getCompareFunc(af.Type(), af.Kind())(af, bf)
-		if c != 0 {
-			return c
-		}
+func getCompareFuncStruct(typ reflect.Type) CompareFunc {
+	f, ok := compareFuncStructCache.Load(typ)
+	if ok {
+		return f
 	}
-	return 0
+	fs := GetStructFields(typ)
+	l := fs.Len()
+	cmps := make([]CompareFunc, l)
+	for i := range l {
+		cmps[i] = GetCompareFunc(fs.Get(i).Type)
+	}
+	f = func(a, b reflect.Value) int {
+		for i := range l {
+			c := cmps[i](a.Field(i), b.Field(i))
+			if c != 0 {
+				return c
+			}
+		}
+		return 0
+	}
+	compareFuncStructCache.Store(typ, f)
+	return f
 }
+
+var compareFuncStructCache syncutil.Map[reflect.Type, CompareFunc]
 
 func compareNil(a, b reflect.Value) (int, bool) {
 	if a.IsNil() {
