6c3d5d07182eaa33afee8d74404fee198edcd8e0
Pierre Durand <pierredurand@gmail.com>
2025-11-27 19:35:45 +0100

    chansutil: add CollectTo()

--- chansutil/chansutil.go
+++ chansutil/chansutil.go
@@ -2,6 +2,7 @@
 package chansutil
 
 import (
+	"context"
 	"iter"
 )
 
@@ -14,4 +15,22 @@ func Iter[C ~chan E, E any](ch C) iter.Seq[E] {
 			}
 		}
 	}
+}
+
+// CollectTo collects elements from an [iter.Seq] and sends them to a channel.
+// If the context is cancelled, it stops collecting and returns the context error.
+func CollectTo[E any](ctx context.Context, it iter.Seq[E], ch chan<- E) error {
+	done := ctx.Done()
+	for e := range it {
+		select {
+		case ch <- e:
+		case <-done:
+		}
+		select {
+		case <-done:
+			return ctx.Err() //nolint:wrapcheck // We want to return the original context error.
+		default:
+		}
+	}
+	return nil
 }
--- chansutil/chansutil_test.go
+++ chansutil/chansutil_test.go
@@ -1,6 +1,7 @@
 package chansutil
 
 import (
+	"context"
 	"slices"
 	"testing"
 
@@ -21,4 +22,26 @@ func TestIter(t *testing.T) {
 	res := slices.Collect(it)
 	expected := []int{2, 3}
 	assert.SliceEqual(t, res, expected)
+}
+
+func TestCollectTo(t *testing.T) {
+	ctx := context.Background()
+	vs := []int{1, 2, 3}
+	it := slices.Values(vs)
+	ch := make(chan int, len(vs))
+	err := CollectTo(ctx, it, ch)
+	assert.NoError(t, err)
+	close(ch)
+	res := slices.Collect(Iter(ch))
+	assert.SliceEqual(t, res, vs)
+}
+
+func TestCollectToContextCancelled(t *testing.T) {
+	ctx, cancel := context.WithCancel(t.Context())
+	cancel()
+	vs := []int{1, 2, 3}
+	it := slices.Values(vs)
+	ch := make(chan int)
+	err := CollectTo(ctx, it, ch)
+	assert.ErrorIs(t, err, context.Canceled)
 }
