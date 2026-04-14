b4ab1d5c18c0fffe85d5b8fdbc241463ac99bef7
Pierre Durand <pierredurand@gmail.com>
2026-04-14 15:55:04 +0200

    panicutil: expose the Error type

--- panicutil/panicutil.go
+++ panicutil/panicutil.go
@@ -8,39 +8,43 @@ import (
 	"github.com/pierrre/go-libs/runtimeutil"
 )
 
-type panicError struct {
-	r       any
-	callers []uintptr
+// Error represents a panic error, which includes the recovered panic value and the stack trace at the time of the panic.
+type Error struct {
+	Recovered any
+	Callers   []uintptr
 }
 
 // NewError creates a new error from a recovered panic value.
 // The error message includes the panic value and the stack trace.
 func NewError(r any) error {
-	return &panicError{
-		r:       r,
-		callers: runtimeutil.GetCallers(1),
+	return &Error{
+		Recovered: r,
+		Callers:   runtimeutil.GetCallers(1),
 	}
 }
 
 // Error implements error.
-func (p *panicError) Error() string {
+func (p *Error) Error() string {
 	buf := bufPool.Get()
 	defer bufPool.Put(buf)
-	_, _ = fmt.Fprint(buf, p.r)
+	_, _ = fmt.Fprint(buf, p.Recovered)
 	_, _ = buf.WriteString("\n\n")
-	fs := runtimeutil.GetCallersFrames(p.callers)
+	fs := runtimeutil.GetCallersFrames(p.Callers)
 	_, _ = runtimeutil.WriteFrames(buf, fs)
 	return buf.String()
 }
 
 // Unwrap returns the wrapped error if the recovered panic value is an error.
-func (p *panicError) Unwrap() error {
-	err, _ := p.r.(error)
+func (p *Error) Unwrap() error {
+	err, _ := p.Recovered.(error)
 	return err
 }
 
-func (p *panicError) StackFrames() []uintptr {
-	return p.callers
+// StackFrames returns the stack frames of the panic.
+//
+// It is used by the Sentry library.
+func (p *Error) StackFrames() []uintptr {
+	return p.Callers
 }
 
 var bufPool = &bufpool.Pool{
